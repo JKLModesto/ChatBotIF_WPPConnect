a285ba32181ac88fb3417541b677ff98e69c1d4c
Pedro Modesto <pedrogabrielmodesto@gmail.com>
2023-10-23 01:34:18 -0300

    Alterações de nome de arquivo

---
+++ base.js
@@ -0,0 +1,24 @@
+
+const wppconnect = require('@wppconnect-team/wppconnect');
+
+wppconnect
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'oi' || message.body === "Oi" && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Teste autobot @WPPConnect')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
