6e7b0715a550d43f3bce4e3f4766e393e96ad265
Pedro Modesto <pedrogabrielmodesto@gmail.com>
2023-10-23 19:26:12 -0300

    Exemplo async no base.js

--- base.js
+++ base.js
@@ -9,16 +9,23 @@ wppconnect
   });
 
 function start(client) {
-  client.onMessage((message) => {
+  client.onMessage(async (message) => {
     if (message.body === 'oi' || message.body === "Oi" && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, 'Teste autobot @WPPConnect')
-        .then((result) => {
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro); //return object error
-        });
+
+      try {
+        let res = await client
+          .sendText(message.from, 'Teste autobot @WPPConnect');
+        console.log('Result: ', res);
+      } catch (erro) {
+        console.error('Error when sending: ', erro); //return object error
+      }
+
+      // .then((result) => {
+      //   console.log('Result: ', result); //return object success
+      // })
+      // .catch((erro) => {
+      //   console.error('Error when sending: ', erro); //return object error
+      // });
     }
   });
 }
