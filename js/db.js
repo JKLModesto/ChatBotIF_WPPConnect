14725cd91f17d392240925bd5c1a37d2d76d1b75
JKLModesto <pedrogabrielmodesto@gmail.com>
2024-02-02 00:43:51 -0300

    Atualizando

---
+++ js/db.js
@@ -0,0 +1,11 @@
+const db = {
+  menuAtendimento: `
+    1. PSU 2024 - Seu futuro é no IFPA 
+    2. Link ao site IFPA 
+    3. Endereços IFPA - Campus Tucuruí
+    4. Video apresentação Ciência da Computação
+    5. Finalizar o atendimento`,
+};
+
+
+module.exports = db
